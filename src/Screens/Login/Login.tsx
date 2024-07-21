5217991318725019a379f7ff8a8fd2e2d128ce58
ZiadNasr <ziadnasr988@gmail.com>
2024-07-21 16:12:06 +0300

    Login first phase

--- src/Screens/Login/Login.tsx
+++ src/Screens/Login/Login.tsx
@@ -1,20 +1,38 @@
-import React from "react";
 import "./Login.css";
+import Logo from "../../assets/badge.png";
 
 function Login() {
   return (
-    <div>
-      <h2>Login</h2>
-      <form>
-        <div>
-          <label htmlFor="email">E-mail</label>
-          <input type="email" id="email" />
+    <div className="Login-Body">
+      <div className="Login-Container">
+        <div className=" Login-Card ">
+          <img src={Logo} alt="Logo" />
+          <h2 className="Login-Title">Welcome Back</h2>
+          <p className="Login-Subtitle">Glad to see you again</p>
+          <p className="Login-Subtitle">Sign in back to your account 👋</p>
+          <form>
+            <div className="Login-Form">
+              <label htmlFor="email" className="text-start">
+                E-mail
+              </label>
+              <input type="email" id="email" />
+            </div>
+            <div className="Login-Form">
+              <label htmlFor="password" className="text-start">
+                Password
+              </label>
+              <input type="email" id="password" />
+            </div>
+            <button className="Login-Button" type="submit">
+              Login
+            </button>
+          </form>
+          <p className="Login-Subtitle">
+            Don't have an account ?{" "}
+            <span className="Login-Signup">Sign up</span> now
+          </p>
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="email" id="password" />
-        </div>
-      </form>
+      </div>
     </div>
   );
 }
