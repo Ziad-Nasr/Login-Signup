de6e4cf8232179192d8f503dad818abb079b8753
ZiadNasr <ziadnasr988@gmail.com>
2024-07-24 00:47:46 +0300

    Errors errors bas

--- src/Components/RadioField.tsx
+++ src/Components/RadioField.tsx
@@ -1,20 +1,30 @@
-import React from "react";
-import { RadioGroup } from "@ark-ui/react";
+import { Field } from "@ark-ui/react";
+import "./InputField.css";
+interface InputFieldProps {
+  label: string;
+  type: string;
+  items: string;
+  value: string;
+  setValue: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
-export const RadioField: React.FC = () => {
-  const frameworks: string[] = ["React", "Solid", "Vue"];
+export default function InputField({
+  label,
+  type,
+  value,
+  setValue,
+}: InputFieldProps) {
+  type Gender = "male" | "female";
+
+  const genders: Gender[] = ["male", "female"];
 
   return (
-    <RadioGroup.Root>
-      <RadioGroup.Label>Framework</RadioGroup.Label>
-      <RadioGroup.Indicator />
-      {frameworks.map((framework) => (
-        <RadioGroup.Item key={framework} value={framework}>
-          <RadioGroup.ItemText>{framework}</RadioGroup.ItemText>
-          <RadioGroup.ItemControl />
-          <RadioGroup.ItemHiddenInput />
-        </RadioGroup.Item>
+    <Field.Root className="inputField d-flex flex-column justify-content-center">
+      <Field.Label className="formLabel">{label}</Field.Label>
+      {genders.map((items) => (
+        <Field.Input value={items} onChange={setValue} type="radio" />
       ))}
-    </RadioGroup.Root>
+      {/* <Field.ErrorText>Error Info</Field.ErrorText> */}
+    </Field.Root>
   );
-};
+}
