dc7b37d68558f3f62a796f1a71e024d24b48fce9
ZiadNasr <ziadnasr988@gmail.com>
2024-07-24 14:09:43 +0300

    First signup page is done

--- src/Components/RadioField.tsx
+++ src/Components/RadioField.tsx
@@ -1,9 +1,8 @@
-import { Field } from "@ark-ui/react";
+import { Field, FieldLabel } from "@ark-ui/react";
 import "./InputField.css";
 interface InputFieldProps {
   label: string;
   type: string;
-  items: string;
   value: string;
   setValue: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
@@ -14,16 +13,31 @@ export default function InputField({
   value,
   setValue,
 }: InputFieldProps) {
-  type Gender = "male" | "female";
+  type Gender = "Male" | "Female";
 
-  const genders: Gender[] = ["male", "female"];
+  const genders: Gender[] = ["Male", "Female"];
 
   return (
     <Field.Root className="inputField d-flex flex-column justify-content-center">
       <Field.Label className="formLabel">{label}</Field.Label>
-      {genders.map((items) => (
-        <Field.Input value={items} onChange={setValue} type="radio" />
-      ))}
+      <div className="container">
+        <div className="row">
+          {genders.map((items) => (
+            <div className="col-6">
+              <Field.Label htmlFor={items} className="me-3">
+                {items}
+              </Field.Label>
+              <Field.Input
+                value={value}
+                id={items}
+                onChange={setValue}
+                type="radio"
+                name="gender"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
       {/* <Field.ErrorText>Error Info</Field.ErrorText> */}
     </Field.Root>
   );
--- src/Screens/SignUp/Signup.tsx
+++ src/Screens/SignUp/Signup.tsx
@@ -1,143 +1,137 @@
-import { useState } from "react";
+import { useState, ChangeEvent } from "react";
 import InputField from "../../Components/InputField";
-import { RadioField } from "../../Components/RadioField";
+import RadioField from "../../Components/RadioField";
 import { useDispatch, useSelector } from "react-redux";
 import Logo from "../../assets/badge.png";
 import "./Signup.css";
 import { formActions } from "../../store/formSlice";
 import { RootState, AppDispatch } from "../../store/store";
 
+interface FormField {
+  label: string;
+  placeholder?: string;
+  type: string;
+  value: string;
+  action: (e: ChangeEvent<HTMLInputElement>) => void;
+  component?: React.ComponentType<any>; // For custom components like RadioField
+}
+
 function Signup() {
-  const dispatch = useDispatch();
+  const dispatch: AppDispatch = useDispatch();
   const formItems = useSelector((state: RootState) => state.myForm);
   const [confirmPassword, setConfirmPassword] = useState("");
 
+  const formFields: FormField[] = [
+    {
+      label: "First Name",
+      placeholder: "First name...",
+      type: "text",
+      value: formItems.firstName,
+      action: (e) =>
+        dispatch(formActions.updateForm({ firstName: e.target.value })),
+    },
+    {
+      label: "Last Name",
+      placeholder: "Last name...",
+      type: "text",
+      value: formItems.lastName,
+      action: (e) =>
+        dispatch(formActions.updateForm({ lastName: e.target.value })),
+    },
+    {
+      label: "Email",
+      placeholder: "Enter Email...",
+      type: "email",
+      value: formItems.email,
+      action: (e) =>
+        dispatch(formActions.updateForm({ email: e.target.value })),
+    },
+    {
+      label: "Phone Number",
+      placeholder: "Enter Phone Number...",
+      type: "text",
+      value: formItems.phoneNumber,
+      action: (e) =>
+        dispatch(formActions.updateForm({ phoneNumber: e.target.value })),
+    },
+    {
+      label: "Password",
+      placeholder: "Enter Password...",
+      type: "password",
+      value: formItems.password,
+      action: (e) =>
+        dispatch(formActions.updateForm({ password: e.target.value })),
+    },
+    {
+      label: "Confirm Password",
+      placeholder: "Confirm Password...",
+      type: "password",
+      value: confirmPassword,
+      action: (e) => setConfirmPassword(e.target.value),
+    },
+    {
+      label: "Gender",
+      type: "radio",
+      value: formItems.gender,
+      action: (e) =>
+        dispatch(formActions.updateForm({ gender: e.target.value })),
+      component: RadioField,
+    },
+  ];
+
   return (
     <div className="Signup-body d-flex justify-content-center align-items-center">
-      <form className="bg-white rounded-4 container">
-        <div className="row">
-          <div className="text-start col-12">
-            <img src={Logo} alt="Logo" />
-            <h2>Signup</h2>
-            <p>Enter your details to signup</p>
+      <div className="PreContainer">
+        <form className="bg-white rounded-4 container">
+          <div className="row">
+            <div className="text-start col-12">
+              <img src={Logo} alt="Logo" />
+              <h2>Signup</h2>
+              <p>Enter your details to signup</p>
+            </div>
           </div>
-        </div>
-        <div className="text-start row">
-          <div className="col-6">
-            <InputField
-              label="First Name"
-              placeholder="First name..."
-              type="text"
-              value={formItems.firstName}
-              setValue={(e) =>
-                dispatch(formActions.updateForm({ firstName: e.target.value }))
-              }
-            />
+          <div className="text-start row">
+            {formFields.map((field, index) => (
+              <div key={index} className="col-6 mt-2">
+                {field.component ? (
+                  <field.component
+                    label={field.label}
+                    type={field.type}
+                    value={field.value}
+                    setValue={field.action}
+                  />
+                ) : (
+                  <InputField
+                    label={field.label}
+                    placeholder={field.placeholder || ""}
+                    type={field.type}
+                    value={field.value}
+                    setValue={field.action}
+                  />
+                )}
+              </div>
+            ))}
           </div>
-          <div className="col-6">
-            <InputField
-              label="Last Name"
-              placeholder="Last name..."
-              type="text"
-              value={formItems.lastName}
-              setValue={(e) =>
-                dispatch(formActions.updateForm({ lastName: e.target.value }))
-              }
-            />
+          <div className="row">
+            <div className="col-12 mt-3">
+              <button className="btn col-3" type="reset">
+                Cancel
+              </button>
+              <button className="btn btn-primary col-3" type="submit">
+                Confirm
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="text-start row">
-          <div className="col-6">
-            <InputField
-              label="Email"
-              placeholder="Enter Email..."
-              type="email"
-              value={formItems.email}
-              setValue={(e) =>
-                dispatch(formActions.updateForm({ email: e.target.value }))
-              }
-            />
+          <div className="row">
+            <div className="col-12 mt-3">
+              <p>
+                Already have an account?{" "}
+                <span className="text-primary">Login</span>
+              </p>
+            </div>
           </div>
-          <div className="col-6">
-            <InputField
-              label="Password"
-              placeholder="Enter Password..."
-              type="password"
-              value={formItems.password}
-              setValue={(e) =>
-                dispatch(formActions.updateForm({ password: e.target.value }))
-              }
-            />
-          </div>
-        </div>
-        <div className="text-start row">
-          <div className="col-6">
-            <InputField
-              label="Confirm Password"
-              placeholder="Confirm Password..."
-              type="password"
-              value={confirmPassword}
-              setValue={(e) => setConfirmPassword(e.target.value)}
-            />
-          </div>
-          <div className="col-6">
-            <RadioField />
-            {/* <InputField
-              label="Gender"
-              placeholder="Select Gender..."
-              type="radio"
-              value={formItems.gender}
-              setValue={(e) =>
-                dispatch(formActions.updateForm({ gender: e.target.value }))
-              } 
-            /> */}
-          </div>
-        </div>
-        <div className="text-start row">
-          <div className="col-6">
-            <InputField
-              label="Email"
-              placeholder="Enter Email..."
-              type="email"
-              value={formItems.email}
-              setValue={(e) =>
-                dispatch(formActions.updateForm({ email: e.target.value }))
-              }
-            />
-          </div>
-          <div className="col-6">
-            <InputField
-              label="Password"
-              placeholder="Enter Password..."
-              type="password"
-              value={formItems.phoneNumber}
-              setValue={(e) =>
-                dispatch(
-                  formActions.updateForm({ phoneNumber: e.target.value })
-                )
-              }
-            />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12 mt-3">
-            <button className="btn col-3" type="reset">
-              Cancel
-            </button>
-            <button className="btn btn-primary col-3" type="submit">
-              Confirm
-            </button>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-12 mt-3">
-            <p>
-              Already have an account?{" "}
-              <span className="text-primary">Login</span>
-            </p>
-          </div>
-        </div>
-      </form>
+        </form>
+      </div>
     </div>
   );
 }
